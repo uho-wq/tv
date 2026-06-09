64b1124a7c02f8a3692192df2b4e8b67e199c3be
kazu-gor <ik126@icloud.com>
2026-06-09 15:16:55 +0900

    feat(main): implement todotxt-viewer CLI entrypoint

---
+++ main.go
@@ -0,0 +1,59 @@
+// Command todotxt-viewer は todo.txt フォーマットのタスクを閲覧する TUI ツール。
+//
+// 使い方:
+//
+//	todotxt-viewer [path]
+//
+// path 省略時は環境変数 (TODOTXT_FILE / TODO_FILE / TODO_DIR) を参照し、
+// 見つからなければ ~/todo.txt を開く。
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/uho-wq/todotxt-viewer/internal/config"
+	"github.com/uho-wq/todotxt-viewer/internal/store"
+	"github.com/uho-wq/todotxt-viewer/internal/ui"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "todotxt-viewer - todo.txt 閲覧 TUI\n\n")
+		fmt.Fprintf(os.Stderr, "Usage: %s [path]\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "path 省略時は TODOTXT_FILE / TODO_FILE / TODO_DIR / ~/todo.txt の順に解決します。\n")
+	}
+	flag.Parse()
+
+	var arg string
+	if flag.NArg() > 0 {
+		arg = flag.Arg(0)
+	}
+
+	if err := run(arg); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func run(arg string) error {
+	path, err := config.ResolvePath(arg)
+	if err != nil {
+		return err
+	}
+
+	f, err := store.Load(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("todo.txt が見つかりません: %s\n引数でパスを指定するか ~/todo.txt を作成してください", path)
+		}
+		return err
+	}
+
+	p := tea.NewProgram(ui.New(f), tea.WithAltScreen())
+	_, err = p.Run()
+	return err
+}
